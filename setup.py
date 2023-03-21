3820bab6cfc39d45226756a912a1258c21fc1737
Justin O'Connor <justinoconnor@Justins-MacBook-Air.local>
2023-03-21 11:27:31 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="terraflow",
-    version="0.1.2",
+    version="0.1.3-beta",
     py_modules=["terraflow"],
     install_requires=[
         "Click",
