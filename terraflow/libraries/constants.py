0e9e7733246e334048114088002aa547e1589222
Justin O'Connor <justinoconnor@Justins-MacBook-Air.local>
2023-07-12 14:23:00 -0400

    add ALLOWED_SCOPES back to constants

--- terraflow/libraries/constants.py
+++ terraflow/libraries/constants.py
@@ -3,6 +3,7 @@ Constant values used across the CLI.
 """
 import os
 
+ALLOWED_SCOPES = ["data_source", "resource", "provider"]
 TERRAFLOW_DIR = ".terraflow"
 DOCUMENTATION_DIR = os.path.join(TERRAFLOW_DIR, "documentation")
 TERRAFORM_REGISTRY_BASE = "registry.terraform.io"
